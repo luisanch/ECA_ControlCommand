005d190be2ce38eae137c4234f29b56d61b816e4
Eduardo-230 <96792014+Eduardo-230@users.noreply.github.com>
2022-01-01 17:37:05 +0100

    Update PilotDepth.m

--- Piloting/Vertical/PilotDepth.m
+++ Piloting/Vertical/PilotDepth.m
@@ -16,16 +16,6 @@ K = zeros(1,5);
 K = lqrd(A,B,Q4,R,in.delta_time_s); %wait for teachers solution
 BARc_rad = (BAR0 + dq*K(1) + dw*K(2) + dz*K(3) + dtheta*K(4) + (Iz - 0)*K(5));
 
-% %% Compute deltas
-% delta_z = in.zc_m - in.z_m;
-% delta_z = EcaF_Saturate(delta_z,-parameters.delta_z_sat_m,parameters.delta_z_sat_m);
-
-% delta_w = 0.0 - in.w_ms;
-% 
-% BARc_rad = parameters.Kp*delta_z + parameters.Ki*memory.int_z*delta_z + parameters.Kd*delta_w;
-% 
-% %% Output saturation
-% out.BARc = EcaF_Saturate(BARc_rad,parameters.BAR_sat_rad(1),parameters.BAR_sat_rad(2));
 out.BARc = EcaF_Saturate(BARc_rad,parameters.BAR_sat_rad(1),parameters.BAR_sat_rad(2));
 
 end
